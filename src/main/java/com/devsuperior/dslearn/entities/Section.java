826a38adc2870ec43ec64ef1fc8af2edf2811f06
Caue Garcia <cauerg7@outlook.com>
2024-04-18 18:22:17 -0300

    Section entity.

--- src/main/java/com/devsuperior/dslearn/entities/Section.java
+++ src/main/java/com/devsuperior/dslearn/entities/Section.java
@@ -1,10 +1,6 @@
 package com.devsuperior.dslearn.entities;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-
-import com.devsuperior.dslearn.entities.enums.ResourceType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,12 +9,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource {
+@Table(name = "tb_section")
+public class Section {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,28 +24,27 @@ public class Resource {
 	
 	@Column(columnDefinition = "TEXT")
 	private String imgUri;
-	private ResourceType type;
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
 	
-	@OneToMany(mappedBy = "resource")
-	private List<Section> sections = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
 	
-	public Resource() {
+	public Section() {
 	}
 
-	public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
-			Offer offer) {
-		super();
+	public Section(Long id, String title, String desciptdescriptionion, Integer position, String imgUri, Resource resource,
+			Section prerequisite) {
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUri = imgUri;
-		this.type = type;
-		this.offer = offer;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
 	}
 
 	public Long getId() {
@@ -73,8 +67,8 @@ public class Resource {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setDesciption(String desciption) {
+		this.description = desciption;
 	}
 
 	public Integer getPosition() {
@@ -93,20 +87,20 @@ public class Resource {
 		this.imgUri = imgUri;
 	}
 
-	public ResourceType getType() {
-		return type;
+	public Resource getResource() {
+		return resource;
 	}
 
-	public void setType(ResourceType type) {
-		this.type = type;
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-	public Offer getOffer() {
-		return offer;
+	public Section getPrerequisite() {
+		return prerequisite;
 	}
 
-	public void setOffer(Offer offer) {
-		this.offer = offer;
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
 	}
 
 	@Override
@@ -122,7 +116,7 @@ public class Resource {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		return Objects.equals(id, other.id);
 	}
 }
